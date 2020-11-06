d59443b4ef08c2734e94b6faaa1c6212240a9486
Oleksii Fomazov <oleksii@fomazov.name>
2020-11-06 10:39:59 +0200

    Changed layout of internal page, fixing deploy

--- src/pages/CommitInfo.jsx
+++ src/pages/CommitInfo.jsx
@@ -3,24 +3,15 @@ import { GithubContext } from '../context/github/githubContext'
 import { Link } from 'react-router-dom'
 
 export const CommitInfo = ({ match }) => {
-  const { getCommit, loading } = useContext(GithubContext)
+  const { getCommit, loading, info } = useContext(GithubContext)
   const urlSha = match.params.sha
 
-  useEffect(async () => {
-    await getCommit(urlSha)
+  useEffect(() => {
+    getCommit(urlSha)
     // eslint-disable-next-line
   }, [])
 
-  const { info } = useContext(GithubContext)
-  const {
-    sha,
-    html_url,
-    author: { avatar_url },
-    commit: {
-      message,
-      author: { name },
-    },
-  } = info
+  const { sha, html_url, url } = info
 
   if (loading) {
     return <p className="text-center">Loading...</p>
@@ -31,17 +22,20 @@ export const CommitInfo = ({ match }) => {
         Back to list
       </Link>
       <div class="card" style={{ width: '18rem' }}>
-        <img class="card-img-top" src={avatar_url} alt="" />
         <div class="card-body">
-          <h5 class="card-title">{name}</h5>
-          <p class="card-text">{message}</p>
+          <h5 class="card-title">{sha}</h5>
+          <p class="card-text">(commmit sha)</p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">{sha}</li>
+          <li class="list-group-item">
+            <a href={url} target="_blank">
+              Click here to view full response
+            </a>
+          </li>
         </ul>
         <div class="card-body">
-          <a href={html_url} class="card-link">
-            Link
+          <a href={html_url} class="card-link" target="_blank">
+            Click here to view commit via GitHub UI
           </a>
         </div>
       </div>
